0489144fc86e828cbb3c5d8795e72c6ee23153bf
Damien Bernard <damien.bernard@doctena.com>
2023-05-05 20:12:18 +0200

    Create data.json file if it doesn't exists yet

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import {Plugin} from 'obsidian'
 import { ListView, VIEW_TYPE } from './view'
+import { accessSync, writeFileSync, readdirSync} from 'fs'
 
 // Remember to rename these classes and interfaces!
 
@@ -7,11 +8,12 @@ import { ListView, VIEW_TYPE } from './view'
 export default class MyPlugin extends Plugin {
 	async onload() {
 		console.log('loading')
+		await this.createDataFile()
 
 		this.registerView(
 			VIEW_TYPE,
 			(leaf) => new ListView(leaf)
-    )
+		)
 
 		this.addCommand({
 			id: "open-view",
@@ -29,12 +31,18 @@ export default class MyPlugin extends Plugin {
 		this.addListView()
 
 		this.registerMarkdownCodeBlockProcessor(
-      'ingredient',
-      async (source, el, ctx) => {
+			'ingredient',
+			async (source, el, ctx) => {
 				const buttonHtml = '<button class="my-button">Add to list</button>'
-        el.innerHTML = buttonHtml
-      }
-    )
+				el.innerHTML = buttonHtml
+			}
+		)
+
+	}
+
+	onunload() {
+		console.log('Unloading')
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE)
 	}
 
 	viewExists () {
@@ -50,9 +58,20 @@ export default class MyPlugin extends Plugin {
 		});
 	}
 
-	onunload() {
-		console.log('Unloading')
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE)
+	async createDataFile() {
+		// console.log(this.app.workspace.getActiveFile()?.parent?.path, this.app.vault.getRoot().vault.adapter.read
+		const basePath = `${(this.app.vault.adapter as any).getBasePath()}/.obsidian/plugins/obsidian-chef/`
+		try {
+			await accessSync(`${basePath}/data.json`);
+			console.log('File already exists!');
+		} catch (error) {
+			if (error.code === 'ENOENT') {
+				console.log('File does not exist. Creating file...');
+				await writeFileSync(`${basePath}/data.json`, '{}');
+				console.log('File created!');
+			} else {
+				throw error
+			}
+		}
 	}
-
 }
